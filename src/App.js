05714c3f5e012d9e8d6102ed01d07eed81daec79
jimmytek <jreynaud@woobaa.fr>
2019-07-14 18:48:27 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Allez Paris !!!!!
+          Neymar dégage !!!!!
         </p>
         <a
           className="App-link"
